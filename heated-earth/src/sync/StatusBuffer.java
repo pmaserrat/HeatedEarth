de798a741689817eeed61b9720fd0e82b3fa9250
Derek Finlinson <dcfinlinson@gmail.com>
2014-10-13 09:54:52 -0600

    Removed duplicate SimConstants

--- heated-earth/src/sync/StatusBuffer.java
+++ heated-earth/src/sync/StatusBuffer.java
@@ -6,32 +6,33 @@ import simulation.SimStatus;
 
 public class StatusBuffer {
 
-        // The buffer data
-        private Queue<SimStatus> bufferQueue;
+	// The buffer data
+	private Queue<SimStatus> bufferQueue;
 
-        private static StatusBuffer instance;
-        public static StatusBuffer getInstance() {
-                if(instance == null) {
-                        instance = new StatusBuffer();
-                }
-                return instance;
-        }
-        public static void destroy() {
-                if(instance != null) {
-                        instance = null;
-                }
-        }
+	private static StatusBuffer instance;
 
-        public StatusBuffer() {
-                this.bufferQueue = new LinkedList<SimStatus>();
-        }
-        
-        public SimStatus pullStatus() {
-                return bufferQueue.poll();
-        }
-        
-        public boolean pushStatus(SimStatus simStatus) {
-                return bufferQueue.offer(simStatus);
-        }
-        
+	public static StatusBuffer getInstance() {
+		if (instance == null) {
+			instance = new StatusBuffer();
+		}
+		return instance;
+	}
+
+	public static void destroy() {
+		if (instance != null) {
+			instance = null;
+		}
+	}
+
+	public StatusBuffer() {
+		this.bufferQueue = new LinkedList<SimStatus>();
+	}
+
+	public SimStatus pullStatus() {
+		return bufferQueue.poll();
+	}
+
+	public boolean pushStatus(SimStatus simStatus) {
+		return bufferQueue.offer(simStatus);
+	}
 }
