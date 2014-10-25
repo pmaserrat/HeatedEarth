7d167843045531db21db7134fe561e5f8c099d68
Derek Finlinson <dcfinlinson@gmail.com>
2014-10-25 09:08:17 -0600

    Added more tests

--- heated-earth/test/EarthSimTest/TimeMemTest.java
+++ heated-earth/test/EarthSimTest/TimeMemTest.java
@@ -6,83 +6,109 @@ import java.io.BufferedWriter;
 import java.io.PrintWriter;
 
 import simulation.SimConstant;
-import userControl.SimOptions;
+import userControl.CommunicationConfig;
+import userControl.SimulationOptions;
+import userControl.ThreadConfig;
 import userControl.UserControl;
 import simulation.SimEngine;
 import junit.framework.TestCase;
 
 class BaseTest {
-        public static void test(int gridSpacing, int simRate, int visRate,
-                        SimOptions.THREAD_CONFIG thread, SimOptions.COMM_CONFIG comm ) {
-                SimConstant.TESTING = true;
-                SimOptions opts = SimOptions.getInstance();
-                opts.setGridSpacing(gridSpacing);
-                opts.setSimulationRate(simRate);
-                opts.setVisualizationRate(visRate);
-                opts.setThreadConfig(thread);
-                opts.setCommConfig(comm);
-                opts.setResetOnStart(true);
-                
-                long starttime = System.currentTimeMillis();
-                
-                new UserControl();
-                opts.setRun(true);      // This starts everything!... user control will see it happen and get things moving.
-                long total = Runtime.getRuntime().totalMemory();
-                try {
-                        Thread.sleep(1000);
-                } catch (InterruptedException e) {                      
-                }
-                long free = Runtime.getRuntime().freeMemory();
-                int steps = SimEngine.getInstance().getSteps();
-                long endtime = System.currentTimeMillis();
-                opts.setRun(false);
+	public static void test(int gridSpacing, int simRate, int visRate,
+			ThreadConfig thread, CommunicationConfig comm) {
+		SimConstant.TESTING = true;
+		SimulationOptions opts = SimulationOptions.getInstance();
+		opts.setGridSpacing(gridSpacing);
+		opts.setSimulationRate(simRate);
+		opts.setVisualizationRate(visRate);
+		opts.setThreadConfig(thread);
+		opts.setCommConfig(comm);
+		opts.setResetOnStart(true);
 
-                long memory = total - free;
-                long time = endtime-starttime;
+		long starttime = System.currentTimeMillis();
 
-                float timePerSteps = (float)time / steps;
-                System.out.println("the memory usage is :" + memory
-                                + " and the runtime per steps is "+ timePerSteps + " Milliseconds.");
+		new UserControl();
+		opts.setRun(true); // This starts everything!... user control will see
+							// it happen and get things moving.
+		long total = Runtime.getRuntime().totalMemory();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		long free = Runtime.getRuntime().freeMemory();
+		int steps = SimEngine.getInstance().getSteps();
+		long endtime = System.currentTimeMillis();
+		opts.setRun(false);
 
-                // Swaroop: ADD WRITE TO CSV FILE FUNCTIONS HERE
-                // OUTPUT is: gridSpacing, simRate, visRate, ThreadConfig, COmmCOnif, mem, timePerSteps
-                try
-                {
-                    String filename= "MyDataFile.csv";
-                    boolean append = true;
-                    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filename, append)));
+		long memory = total - free;
+		long time = endtime - starttime;
 
-                    out.print(gridSpacing);
-                    out.print(",");
-                    out.print(simRate);
-                    out.print(",");
-                    out.print(visRate);
-                    out.print(",");
-                    out.print(thread);
-                    out.print(",");
-                    out.print(comm);
-                    out.print(",");
-                    out.print(memory);
-                    out.print(",");
-                    out.print(timePerSteps);
-                    out.print(",");
-                    out.println();
-                    out.close();
-                }
+		float timePerSteps = (float) time / steps;
+		System.out.println("the memory usage is :" + memory
+				+ " and the runtime per steps is " + timePerSteps
+				+ " Milliseconds.");
 
-                catch(IOException ioe)
-                {
-                    System.err.println("IOException: " + ioe.getMessage());
-                } 
+		// Swaroop: ADD WRITE TO CSV FILE FUNCTIONS HERE
+		// OUTPUT is: gridSpacing, simRate, visRate, ThreadConfig, COmmCOnif,
+		// mem, timePerSteps
+		try {
+			String filename = "MyDataFile.csv";
+			boolean append = true;
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new FileWriter(filename, append)));
 
-        }
+			out.print(gridSpacing);
+			out.print(",");
+			out.print(simRate);
+			out.print(",");
+			out.print(visRate);
+			out.print(",");
+			out.print(thread);
+			out.print(",");
+			out.print(comm);
+			out.print(",");
+			out.print(memory);
+			out.print(",");
+			out.print(timePerSteps);
+			out.print(",");
+			out.println();
+			out.close();
+		}
+
+		catch (IOException ioe) {
+			System.err.println("IOException: " + ioe.getMessage());
+		}
+
+	}
 }
-public class TimeMemTest  extends TestCase {
 
-        public void test_Buffer_Pres() {
-                BaseTest.test(1, 10, 1, 
-                                SimOptions.THREAD_CONFIG.PRES_ONLY, SimOptions.COMM_CONFIG.BUFFER);
-                BaseTest.test(1, 5, 4, 
-                              SimOptions.THREAD_CONFIG.PRES_ONLY, SimOptions.COMM_CONFIG.BUFFER);
-        }
+public class TimeMemTest extends TestCase {
+
+	public void test_Buffer_None() {
+		BaseTest.test(1, 10, 1, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+		BaseTest.test(1, 5, 4, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+	}
+
+	public void test_Buffer_Pres() {
+		BaseTest.test(1, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(1, 5, 4, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(1, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.BUFFER);
+	}
+
+	public void test_Buffer_Simulation() {
+		BaseTest.test(1, 10, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(1, 5, 4, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.BUFFER);
+	}
+
+	public void test_Buffer_Both() {
+		BaseTest.test(1, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(1, 5, 4, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.BUFFER);
+	}
 }
