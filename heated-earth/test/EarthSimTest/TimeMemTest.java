0c7b789138e01173d64cb8bd19edb649a3ef5dcf
Derek Finlinson <dcfinlinson@gmail.com>
2014-10-25 09:53:49 -0600

    Added all tests. Added new scenarios to Chart

--- heated-earth/test/EarthSimTest/TimeMemTest.java
+++ heated-earth/test/EarthSimTest/TimeMemTest.java
@@ -84,31 +84,228 @@ class BaseTest {
 
 public class TimeMemTest extends TestCase {
 
-	public void test_Buffer_None() {
-		BaseTest.test(1, 10, 1, ThreadConfig.NONE, CommunicationConfig.BUFFER);
-		BaseTest.test(1, 5, 4, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+	public void test_Scenario_1() {
+		AppendToFile("Scenario 1");
+
+		BaseTest.test(15, 10, 1, ThreadConfig.NONE, CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PUSH);
+
+		BaseTest.test(15, 10, 1, ThreadConfig.NONE, CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PULL);
+
+		BaseTest.test(15, 10, 1, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.BUFFER);
 	}
 
-	public void test_Buffer_Pres() {
-		BaseTest.test(1, 10, 1, ThreadConfig.PRESENTATIONONLY,
+	public void test_Scenario_2() {
+		AppendToFile("Scenario 2");
+
+		BaseTest.test(10, 10, 1, ThreadConfig.NONE, CommunicationConfig.PUSH);
+		BaseTest.test(10, 10, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(10, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(10, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PUSH);
+
+		BaseTest.test(10, 10, 1, ThreadConfig.NONE, CommunicationConfig.PULL);
+		BaseTest.test(10, 10, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(10, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(10, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PULL);
+
+		BaseTest.test(10, 10, 1, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+		BaseTest.test(10, 10, 1, ThreadConfig.SIMULATIONONLY,
 				CommunicationConfig.BUFFER);
-		BaseTest.test(1, 5, 4, ThreadConfig.PRESENTATIONONLY,
+		BaseTest.test(10, 10, 1, ThreadConfig.PRESENTATIONONLY,
 				CommunicationConfig.BUFFER);
-		BaseTest.test(1, 10, 1, ThreadConfig.PRESENTATIONONLY,
+		BaseTest.test(10, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
 				CommunicationConfig.BUFFER);
 	}
 
-	public void test_Buffer_Simulation() {
-		BaseTest.test(1, 10, 1, ThreadConfig.SIMULATIONONLY,
+	public void test_Scenario_3() {
+		AppendToFile("Scenario 3");
+
+		BaseTest.test(20, 10, 1, ThreadConfig.NONE, CommunicationConfig.PUSH);
+		BaseTest.test(20, 10, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(20, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(20, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PUSH);
+
+		BaseTest.test(20, 10, 1, ThreadConfig.NONE, CommunicationConfig.PULL);
+		BaseTest.test(20, 10, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(20, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(20, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PULL);
+
+		BaseTest.test(20, 10, 1, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+		BaseTest.test(20, 10, 1, ThreadConfig.SIMULATIONONLY,
 				CommunicationConfig.BUFFER);
-		BaseTest.test(1, 5, 4, ThreadConfig.SIMULATIONONLY,
+		BaseTest.test(20, 10, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(20, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
 				CommunicationConfig.BUFFER);
 	}
 
-	public void test_Buffer_Both() {
-		BaseTest.test(1, 10, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+	public void test_Scenario_4() {
+		AppendToFile("Scenario 4");
+
+		BaseTest.test(15, 15, 1, ThreadConfig.NONE, CommunicationConfig.PUSH);
+		BaseTest.test(15, 15, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 15, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 15, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PUSH);
+
+		BaseTest.test(15, 15, 1, ThreadConfig.NONE, CommunicationConfig.PULL);
+		BaseTest.test(15, 15, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 15, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 15, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PULL);
+
+		BaseTest.test(15, 15, 1, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+		BaseTest.test(15, 15, 1, ThreadConfig.SIMULATIONONLY,
 				CommunicationConfig.BUFFER);
-		BaseTest.test(1, 5, 4, ThreadConfig.SIMULATIONANDPRESENTATION,
+		BaseTest.test(15, 15, 1, ThreadConfig.PRESENTATIONONLY,
 				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 15, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.BUFFER);
+	}
+
+	public void test_Scenario_5() {
+		AppendToFile("Scenario 5");
+
+		BaseTest.test(15, 5, 1, ThreadConfig.NONE, CommunicationConfig.PUSH);
+		BaseTest.test(15, 5, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 5, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 5, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PUSH);
+
+		BaseTest.test(15, 5, 1, ThreadConfig.NONE, CommunicationConfig.PULL);
+		BaseTest.test(15, 5, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 5, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 5, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PULL);
+
+		BaseTest.test(15, 5, 1, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+		BaseTest.test(15, 5, 1, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 5, 1, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 5, 1, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.BUFFER);
+	}
+
+	public void test_Scenario_6() {
+		AppendToFile("Scenario 6");
+
+		BaseTest.test(15, 10, 3, ThreadConfig.NONE, CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 3, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 3, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 3, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PUSH);
+
+		BaseTest.test(15, 10, 3, ThreadConfig.NONE, CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 3, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 3, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 3, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PULL);
+
+		BaseTest.test(15, 10, 3, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 3, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 3, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 3, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.BUFFER);
+	}
+
+	public void test_Scenario_7() {
+		AppendToFile("Scenario 7");
+
+		BaseTest.test(15, 10, 5, ThreadConfig.NONE, CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 5, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 5, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PUSH);
+		BaseTest.test(15, 10, 5, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PUSH);
+
+		BaseTest.test(15, 10, 5, ThreadConfig.NONE, CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 5, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 5, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.PULL);
+		BaseTest.test(15, 10, 5, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.PULL);
+
+		BaseTest.test(15, 10, 5, ThreadConfig.NONE, CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 5, ThreadConfig.SIMULATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 5, ThreadConfig.PRESENTATIONONLY,
+				CommunicationConfig.BUFFER);
+		BaseTest.test(15, 10, 5, ThreadConfig.SIMULATIONANDPRESENTATION,
+				CommunicationConfig.BUFFER);
+	}
+
+	private void AppendToFile(String scenario) {
+		String filename = "MyDataFile.csv";
+		boolean append = true;
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new FileWriter(filename, append)));
+
+			out.print(scenario);
+			out.println();
+			out.print("Grid Spacing");
+			out.print(",");
+			out.print("Simulation Rate");
+			out.print(",");
+			out.print("Visulation Rate");
+			out.print(",");
+			out.print("Thread Config");
+			out.print(",");
+			out.print("Communication Config");
+			out.print(",");
+			out.print("Memory");
+			out.print(",");
+			out.print("Time");
+			out.println();
+			out.close();
+		} catch (IOException ioe) {
+			System.err.println("IOException: " + ioe.getMessage());
+		}
 	}
 }
